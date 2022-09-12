42f380eae8cd5d14dd5a3ba133f6d82ee915d76d
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-12 15:21:51 +0400

    add makeRequest module

--- scraper/main.py
+++ scraper/main.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from schdef import Communique
 from cleanstring import cleanString
+from emailsender import sendEmail
 
 LAST_SCRAPED_COMMUNIQUE = {}  # communique since last time scraping was done
 DATABASE_FILE_PATH = "data/scrape.json" # file containing LAST_SCRAPED_COMMUNIQUE
@@ -15,29 +16,29 @@ DATABASE_FILE_PATH = "data/scrape.json" # file containing LAST_SCRAPED_COMMUNIQU
 #     with open(TEST_FILE_PATH, 'a', encoding='utf-8') as f:
 #         json.dump(communique_info, f, ensure_ascii=False, indent=4)
 
+def makeRequest(URL):
+    HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+    }
+    TIMEOUT = 100 # allow at most 100s to make request
+    try:
+        r = requests.get(URL, headers=HEADERS, timeout=TIMEOUT)
+    except Exception as e:
+        raise SystemExit(e)
+    else:
+        if (r.status_code == 200): # valid response
+            return r
+        raise SystemExit("Invalid status code when requesting PDF : {r.status_code}", "\n URL : {URL}")
+
 def main():
     global LAST_SCRAPED_COMMUNIQUE
     # Initialise LAST_SCRAPED_COMMUNIQUE
     with open(DATABASE_FILE_PATH) as f:
         LAST_SCRAPED_COMMUNIQUE = json.load(f)
 
-    # call website
-    HEADERS = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-    }
+    # scrape website
     URL = "https://education.govmu.org/Pages/Downloads/Scholarships/Scholarships-for-Mauritius-Students.aspx"
-
-    try:
-        r = requests.get(URL, headers=HEADERS, timeout=100)
-    except requests.exceptions.Timeout as e:
-    # we didn't get a reply
-        print("Request timed out!\nDetails:", e)
-    else:
-        if (r.status_code == 200): # valid response
-            scrapeWebsite(r.text)
-            return
-        print("ERROR : Failed to request website : {r.status_code}")
-
+    scrapeWebsite(makeRequest(URL).text)
 
 def updateDatabase(communique_info):
     with open(DATABASE_FILE_PATH, 'w', encoding='utf-8') as f:
@@ -97,7 +98,7 @@ def scrapeWebsite(RESPONSETEXT):
             first_communique = current_communique.to_dict()
 
         print(current_communique.title, "\n")
-        # Send email
+        sendEmail(current_communique.title, current_communique.to_dict(),'c34560814@gmail.com')
     print(count, "new scholarships discovered !")
 
 main()
