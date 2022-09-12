a301c470140e729881907fadce2b86756bbb78f9
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-12 20:55:22 +0400

    refactor code

--- scraper/emailsender.py
+++ scraper/emailsender.py
@@ -5,14 +5,9 @@ from dotenv import load_dotenv, find_dotenv
 import datetime
 
 def sendEmail(emailSubject, emailBodyhtml, recipient):
-    # for local testing
     load_dotenv(find_dotenv())
     gmail.password  = os.getenv('EMAIL_PASSCODE')
-    # print(os.getenv('EMAIL_PASSCODE'))
-
     gmail.username = 'c34560814@gmail.com' 
-    # for github actions 
-    # gmail.password = os.environ['EMAIL_PASSCODE']
 
     gmail.send(
         subject=emailSubject,
@@ -24,4 +19,4 @@ if __name__ == "__main__":
     emailSubject = ('{:%Y-%m-%d %H:%M:%S}'.
                     format(datetime.datetime.now()))
     recipient = 'c34560814@gmail.com'
-    sendEmail(emailSubject,"tset",recipient)
+    sendEmail(emailSubject,"this is a test",recipient)
