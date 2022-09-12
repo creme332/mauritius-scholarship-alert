ae1cfe037819bdf8e11aee0c8234bb90fe2b4066
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-12 17:50:27 +0400

    use env variable in emailsender

--- scraper/emailsender.py
+++ scraper/emailsender.py
@@ -1,18 +1,23 @@
 #!venv/bin/python3
 from redmail import gmail
-import datetime
 import os
+from dotenv import load_dotenv, find_dotenv
 
 def sendEmail(emailSubject, emailBodyhtml, recipient):
+    # for local testing
+    # load_dotenv(find_dotenv())
+    # gmail.password  = os.getenv('EMAIL_PASSCODE')
+    # print(os.getenv('EMAIL_PASSCODE'))
+
     gmail.username = 'c34560814@gmail.com' 
-    gmail.password = os.environ.get('EMAIL_PASSCODE')
+    # for github actions 
+    gmail.password = os.environ['EMAIL_PASSCODE']
 
     gmail.send(
         subject=emailSubject,
         receivers=recipient,
-        html=emailBodyhtml
+        text=emailBodyhtml
     )
-
 # emailSubject = ('{:%Y-%m-%d %H:%M:%S}'.
 #                  format(datetime.datetime.now()))
 # recipientList = ['c34560814@gmail.com','cosmiczorve@gmail.com']
