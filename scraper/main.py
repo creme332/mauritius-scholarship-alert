20bf4c9298000a151c565c3f76365cd6093052c7
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-13 10:41:36 +0400

    refactor main.py to deal with a new case
    main.py now solves the case where website was never scraped and lastcommunique is empty

--- scraper/main.py
+++ scraper/main.py
@@ -9,13 +9,9 @@ from pdfreader import getPDFtext
 from requestfunc import makeRequest
 
 LAST_SCRAPED_COMMUNIQUE = {}  # communique since last time scraping was done
-DATABASE_FILE_PATH = "data/scrape.json" # file containing LAST_SCRAPED_COMMUNIQUE
+# file containing LAST_SCRAPED_COMMUNIQUE
+DATABASE_FILE_PATH = "data/scrape.json"
 
-# For testing purposes
-# def addToTestDB(communique_info):
-#     TEST_FILE_PATH =  "data/test.json"
-#     with open(TEST_FILE_PATH, 'a', encoding='utf-8') as f:
-#         json.dump(communique_info, f, ensure_ascii=False, indent=4)
 
 def main():
     global LAST_SCRAPED_COMMUNIQUE
@@ -27,13 +23,14 @@ def main():
     URL = "https://education.govmu.org/Pages/Downloads/Scholarships/Scholarships-for-Mauritius-Students.aspx"
     scrapeWebsite(makeRequest(URL).text)
 
+
 def updateDatabase(communique_info):
     with open(DATABASE_FILE_PATH, 'w', encoding='utf-8') as f:
         json.dump(communique_info, f, ensure_ascii=False, indent=4)
 
 
 def scrapeWebsite(RESPONSETEXT):
-    BASE_URL = 'https://education.govmu.org' # base url for pdf docs
+    BASE_URL = 'https://education.govmu.org'  # base url for pdf docs
     firstrowfound = False
     first_communique = {}
     global LAST_SCRAPED_COMMUNIQUE
@@ -41,11 +38,11 @@ def scrapeWebsite(RESPONSETEXT):
     soup = BeautifulSoup(RESPONSETEXT, 'lxml')
 
     # There are 2 tables on the page. Only the first one is important.
-    # We are scraping the newest rows/scholarships first
+    # The first table contains communiques sorted by date (earliest first)
     table = soup.find('table')
     table_rows = table.find_all('tr')
+    newScholarshipsList = []  # list of new scholarships discovered
 
-    count = 0
     for row in table_rows:
         if row.find('td') is None:  # ignore header and footer rows
             continue
@@ -56,19 +53,20 @@ def scrapeWebsite(RESPONSETEXT):
         communique_field = row.find_all('td')[0]
         closingDate_field = row.find_all('td')[1]
 
-        current_communique.title = cleanString(communique_field.find('a').text)
-        # corner case for "Queen Elizabeth Commonwealth Scholarships for academic Year 2022/2023"
+        current_communique.title = cleanString(communique_field.
+                                               find('a').text)
+
+        # The first anchor tag in each row is the scholarship name,
+        #  except for QEC scholarship
+        # corner case for communique similar to QEC
         if (current_communique.title == ""):
             current_communique.title = cleanString(communique_field.text)
 
-        # check if we are done scraping all new communique
-        if (current_communique.title == LAST_SCRAPED_COMMUNIQUE['title']):
-            if(first_communique != {}):
-                # we found at least 1 new scholarship
-                updateDatabase(first_communique)
+        # if we encounter a previously scraped communique, exit
+        if (LAST_SCRAPED_COMMUNIQUE != {} and current_communique.title == LAST_SCRAPED_COMMUNIQUE['title']):
             break
 
-        # keep scraping
+        # create list of links present in current communique
         all_anchor_tags = communique_field.find_all('a')
         urls = []
         for tag in all_anchor_tags:
@@ -78,15 +76,27 @@ def scrapeWebsite(RESPONSETEXT):
         current_communique.closingDate = cleanString(closingDate_field.text)
         current_communique.timestamp = ('{:%Y-%m-%d %H:%M:%S}'.
                                         format(datetime.datetime.now()))
-        count += 1
+        newScholarshipsList.append(current_communique.title)
 
         if not firstrowfound:
             firstrowfound = True
             first_communique = current_communique.to_dict()
 
-        print(current_communique.title, "\n")
-        sendEmail(current_communique.title, getPDFtext(urls[0]),'c34560814@gmail.com')
-    print(count, "new scholarships discovered !")
+        # sendEmail(current_communique.title, getPDFtext(
+        #     urls[0]), 'c34560814@gmail.com')
+    # print updates
+    print(len(newScholarshipsList), "new scholarships discovered !\n")
+    print("\n\n".join(newScholarshipsList))
+
+    # update database only if needed
+    if (LAST_SCRAPED_COMMUNIQUE == {}):  # this is our first time scraping
+        updateDatabase(first_communique)
+    else:
+        if (first_communique != {} and
+                first_communique['title'] != LAST_SCRAPED_COMMUNIQUE['title']):
+            # we have found at least 1 new scholarship
+            updateDatabase(first_communique)
+
 
 if __name__ == "__main__":
     main()
