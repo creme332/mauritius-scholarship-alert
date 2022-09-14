967eed4b38c2a238412db65b39893b939077f8f5
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-14 10:38:03 +0400

    fix typo for email sender address

--- scraper/emailsender.py
+++ scraper/emailsender.py
@@ -7,10 +7,10 @@ import datetime
 def sendEmail(emailSubject, emailBodyhtml):
     # setup credentials
     load_dotenv(find_dotenv())
-    if(os.getenv('SENDER_EMAIL_ADDRESSS') is None):
-        raise SystemExit("SENDER_EMAIL_ADDRESSS not found!")
-    gmail.username = os.getenv('SENDER_EMAIL_ADDRESSS')
-    
+    if(os.getenv('SENDER_EMAIL_ADDRESS') is None):
+        raise SystemExit("SENDER_EMAIL_ADDRESS not found!")
+    gmail.username = os.getenv('SENDER_EMAIL_ADDRESS')
+
     if(os.getenv('EMAIL_PASSCODE') is None):
         raise SystemExit("EMAIL_PASSCODE not found!")
     gmail.password  = os.getenv('EMAIL_PASSCODE')
@@ -20,7 +20,7 @@ def sendEmail(emailSubject, emailBodyhtml):
     try:
         gmail.send(
             subject=emailSubject,
-            receivers=os.getenv('SENDER_EMAIL_ADDRESSS'),
+            receivers=os.getenv('SENDER_EMAIL_ADDRESS'),
             text=emailBodyhtml
         )
     except Exception as e:
