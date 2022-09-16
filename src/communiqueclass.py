e6975293098e86e5e456d74c07f994be9edc0831
creme332 <65414576+creme332@users.noreply.github.com>
2022-09-16 12:31:33 +0400

    remove keyword attribute from communique clas

--- src/communiqueclass.py
+++ src/communiqueclass.py
@@ -1,9 +1,8 @@
 class Communique(object):
-    def __init__(self, title="", closingDate="", timestamp="", urls=[], keywords=[]):
+    def __init__(self, title="", closingDate="", timestamp="", urls=[]):
         self.title = title  # main title as displayed on website
         self.closingDate = closingDate  # as displayed on website
         self.urls = urls  # all links available for current communique
-        self.keywords = keywords
         self.timestamp = timestamp  # time at which communique was scraped
 
     def to_dict(self):
