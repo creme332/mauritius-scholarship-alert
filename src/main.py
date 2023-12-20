ab216cc8503b048a9cacacd0acad87998af583ff
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-20 16:02:59 +0400

    fix issues

--- src/main.py
+++ src/main.py
@@ -16,18 +16,35 @@ from scraper.request_helper import request_all
 DATABASE_FILE_PATH = "data/scrape.json"
 
 
-def filter(all_communiques: list[Communique]) -> list[Communique]:
+def filter_new(all_communiques: list[Communique]) -> list[Communique]:
+    """
+    Given a list of communiques ordered by closing date
+    (oldest communique is last), return the communiques which were
+    not encountered the last time the website was scraped.
+
+    Args:
+        all_communiques (list[Communique]): _description_
+
+    Returns:
+        list[Communique]: _description_
+    """
     new_communiques = []
 
-    last_communique = {}  # communique since last time scraping was done
+    # read last scraped communique from file
+    # ! file is guaranteed to contain at least an empty dictionary
+    last_scraped_communique = {}
     with open(DATABASE_FILE_PATH) as f:
-        last_communique = json.load(f)
+        last_scraped_communique = json.load(f)
 
-    # compare last scraped communique with each communique in list
-    # keep only communiques which are found before last communique in
-    # all_communiques
+    # if website is being scraped for the first time, all communiques are new
+    if not last_scraped_communique:
+        return all_communiques
+
+    # compare the last scraped communique with each communique
+    # in all_communiques. keep only communiques which are found before
+    # the last communique
     for communique in all_communiques:
-        if communique.title == last_communique['title']:
+        if communique.title == last_scraped_communique['title']:
             return new_communiques
         new_communiques.append(communique)
 
@@ -38,25 +55,28 @@ def main() -> None:
     # fetch all communiques from website
     all_communiques = get_all_communiques()
 
+    # filter out communiques which were already scraped before
+    # to get only new scholarships
+    new_communiques = filter_new(all_communiques)
+
     # send reminders if any
     send_reminders(all_communiques)
 
-    # filter out communiques which were already scraped before
-    # to get only new scholarships
-    all_communiques = filter(all_communiques)
-
-    print("Number of new scholarships =", len(all_communiques))
+    print("Number of new scholarships =", len(new_communiques))
 
     # if no new communiques found, exit
-    if (len(all_communiques) == 0):
+    if (len(new_communiques) == 0):
         return
 
+    # update last scraped communique
+    save_recent_communique(new_communiques[0])
+
     # extract communique titles
-    email_titles = [c.title for c in all_communiques]
+    email_titles = [c.title for c in new_communiques]
 
     # * NOTE: each communique has at least 1 url
     # extract first url for each communiques
-    pdf_urls = [c.urls[0] for c in all_communiques]
+    pdf_urls = [c.urls[0] for c in new_communiques]
 
     # request pdfs
     pdf_responses = asyncio.run(request_all(pdf_urls))
@@ -64,6 +84,7 @@ def main() -> None:
     # get the pdf text from each response
     pdfs_text = [extract_text(r) for r in pdf_responses]
 
+    # ! TAKE filter.txt into consideration
     # For newly discovered scholarships, send emails
     emailer = Emailer()
     for i in range(0, min(emailer.EMAIL_LIMIT, len(email_titles))):
@@ -86,7 +107,6 @@ def save_recent_communique(new_communique: Communique) -> None:
 
 if __name__ == "__main__":
     main()
-
     # * Uncomment lines below to measure code performance
     # with cProfile.Profile() as pr:
     #     main()
