e46b4efe1664bbaa18eea94011475750595af221
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-21 08:07:40 +0400

    fix issue when file not found, add filter_new method

--- src/communique_manager.py
+++ src/communique_manager.py
@@ -14,10 +14,15 @@ class CommuniqueManager:
         last_scraped_communique = None
 
         with open(self.file_name, 'r') as f:
-            x = json.load(f)
-            last_scraped_communique = Communique(
-                x['title'], x['closing_date'], x['urls'])
-            return last_scraped_communique
+            try:
+                x = json.load(f)
+                if x:
+                    last_scraped_communique = Communique(
+                        x['title'], x['closing_date'], x['urls'])
+                    return last_scraped_communique
+                return None
+            except json.decoder.JSONDecodeError:
+                return None
 
     def save(self, new_communique: Communique):
         """Saves the most recently scraped communique to `scrape.json`
@@ -29,3 +34,37 @@ class CommuniqueManager:
         with open(self.file_name, 'w', encoding='utf-8') as f:
             json.dump(new_communique.to_dict(), f,
                       ensure_ascii=False, indent=4)
+
+    def filter_new(self,
+                   all_communiques: list[Communique]) -> list[Communique]:
+        """
+        Given a list of communiques scraped from top to bottom of website,
+        return the communiques which were
+        not encountered the last time the website was scraped.
+
+        Args:
+            all_communiques (list[Communique]): _description_
+
+        Returns:
+            list[Communique]: _description_
+        """
+        new_communiques = []
+
+        # read last scraped communique from file
+        last_scraped_communique = self.get_last_communique()
+
+        # if website is being scraped for the first time,
+        # last_scraped_communique will be None so all scraped communiques
+        # are new
+        if not last_scraped_communique:
+            return all_communiques
+
+        # compare the last scraped communique with each communique
+        # in all_communiques. keep only communiques which are found before
+        # the last communique
+        for communique in all_communiques:
+            if communique.title == last_scraped_communique.title:
+                return new_communiques
+            new_communiques.append(communique)
+
+        return new_communiques
