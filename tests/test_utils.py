23d14cb1f139e6457d1067a1ce460c93682aa4f2
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-22 13:22:20 +0400

    add tests for clean_string

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+from src.utils import clean_string
+
+
+class TestCleanString:
+
+    def test_empty_string(self):
+        assert (clean_string("") == "")
+
+    def test_string_with_leading_trailing_spaces(self):
+        assert (clean_string("  hello world") == "hello world")
+
+    def test_string_with_special_chars(self):
+        assert (clean_string(
+            "\xa0\n  hello\xa0world\r\n      \u200b") == "hello world")
