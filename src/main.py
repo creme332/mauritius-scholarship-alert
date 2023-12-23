536c294d10fa8fad12645b4e685e1ef246826639
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-23 13:52:23 +0400

    fix bug when populating atom feed, create a reset function

--- src/main.py
+++ src/main.py
@@ -8,11 +8,45 @@ from scraper.request_helper import request_all
 from feed import Feed
 
 
+def reset_data() -> None:
+    """
+    Call this function when you messed up and you want to reset everything.
+    The following actions are taken:
+    - The atom feed will be erased and initialized with the 5
+    latest scholarships.
+    - The data in scrape.json will get updated.
+    - No emails are sent.
+    - No reminders are sent.
+    """
+    feed_manager = Feed()
+    communique_manager = CommuniqueManager()
+    SIZE = 5
+
+    # erase last scraped communique in scrape.json
+    communique_manager.reset_last_communique()
+
+    # erase atom feed info
+    feed_manager.reset()
+
+    # fetch the 5 latest scholarships and their PDF text
+    all_communiques = get_all_communiques(SIZE)
+    communique_manager.save(all_communiques[0])
+
+    pdf_urls = [c.urls[0] for c in all_communiques]
+    pdf_responses = asyncio.run(request_all(pdf_urls))
+    pdfs_text = [extract_text(r)
+                 for r in pdf_responses if r.status_code == 200]
+
+    # populate atom feed
+    for i in range(len(all_communiques) - 1, -1, -1):
+        feed_manager.add_entry(all_communiques[i], pdfs_text[i])
+
+
 def main() -> None:
     """
     Driver function for program.
     """
-    MANAGER = CommuniqueManager()
+    communique_manager = CommuniqueManager()
 
     # fetch all communiques from website
     print("Fetching all communiques...")
@@ -28,7 +62,7 @@ def main() -> None:
 
     # remove communiques which were already scraped before
     # to get only new communiques
-    new_communiques = MANAGER.get_new_communiques(all_communiques)
+    new_communiques = communique_manager.get_new_communiques(all_communiques)
 
     print(f"{len(new_communiques)} new communiques found.")
 
@@ -56,7 +90,7 @@ def main() -> None:
     print(f"Done. {len(pdfs_text)} PDFs extracted.")
 
     # get user interests
-    user_interests = MANAGER.get_user_interests()
+    user_interests = communique_manager.get_user_interests()
 
     # For send emails
     print("Sending email...")
@@ -68,12 +102,15 @@ def main() -> None:
     print(f"Done. {emailer.sent_count} scholarship emails were sent!")
 
     # update last scraped communique
-    MANAGER.save(new_communiques[0])
+    communique_manager.save(new_communiques[0])
 
     # update feed
     print("Updating feed...")
     feed_manager = Feed()
-    for i in range(0, len(new_communiques)):
+
+    # feed entries must be added in chronological order where
+    # the first entry is the latest one
+    for i in range(len(new_communiques) - 1, -1, -1):
         feed_manager.add_entry(new_communiques[i], pdfs_text[i])
     print(f"Done. Feed size is {feed_manager.get_total_feed_entries()}")
 
